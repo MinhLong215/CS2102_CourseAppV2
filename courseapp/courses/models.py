76e96fd9edb87f75046ae798c210c734bc8ac38b
MinhLong215 <2151010201long@ou.edu.vn>
2023-10-22 16:54:58 +0700

    create all

---
+++ courseapp/courses/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=False)
